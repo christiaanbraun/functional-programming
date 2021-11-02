ec7c10faf3cb2e10cf73e5b2fd118cb7f3d75c26
Christiaan Braun <christiaan.braun@hva.nl>
2021-11-02 15:19:16 +0100

    Make it look sexy

--- script.js
+++ script.js
@@ -15,35 +15,40 @@ function removeExtraWords(string) {
 }
 
 function pushItemsIntoArray(input) {
-	let wins = []
-	for (let i = 0; i < input.nobelPrizes.length; i++) {
-		wins.push(input.nobelPrizes[i])
-	}
-	return wins
-	// console.log(echteData.nobelPrizes[0].laureates[0].knownName.en)
-}
-
-function getLaureatesInfo(input) {
-	let laureates = []
-	for (let i = 0; i < input.length; i++) {
-		laureates.push(input[i].laureates)
-	}
-	return laureates
-}
-
-function putNamesInotArray(input) {
 	let winnerNames = []
-	for (let i = 0; i < input.length; i++) {
-		winnerNames.push(input[i][0].knownName)
+	for (let i = 0; i < input.nobelPrizes.length; i++) {
+		for (let j = 0; j < input.nobelPrizes[i].laureates.length; j++) {
+			if (input.nobelPrizes[i].laureates[j].knownName) {
+				winnerNames.push(input.nobelPrizes[i].laureates[j].knownName.en)
+			} else if (input.nobelPrizes[i].laureates[j].orgName) {
+				winnerNames.push(input.nobelPrizes[i].laureates[j].orgName.en)
+			}
+		}
 	}
+
 	return winnerNames
 }
+
+// Eerst deed ik dit maar dat was omslachtig
+// function getLaureatesInfo(input) {
+// 	let laureates = []
+// 	for (let i = 0; i < input.length; i++) {
+// 		laureates.push(input[i].laureates)
+// 	}
+// 	return laureates
+// }
+
+// function putNamesInotArray(input) {
+// 	let winnerNames = []
+// 	for (let i = 0; i < input.length; i++) {
+// 		winnerNames.push(input[i][0].knownName)
+// 	}
+// 	return winnerNames
+// }
 
 fetch(
 	'https://api.nobelprize.org/2.0/nobelPrizes?limit=200&nobelPrizeYear=1999'
 )
 	.then(response => response.json())
 	.then(data => pushItemsIntoArray(data))
-	.then(data => getLaureatesInfo(data))
-	.then(data => putNamesInotArray(data))
 	.then(data => console.log(data))
